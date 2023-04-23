de3945503194ad3bb3de4bb9fd69dc9a8c47bc00
lromeraj <lromerajdev@gmail.com>
2023-04-23 13:28:10 +0200

    Updated tele bot

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps: [{
     name: 'Iridium SBD',
     script: 'build/src/index.js',
+    args: '-vvv',
     watch: false,
     autorestart: true,
     max_memory_restart: '100M'
